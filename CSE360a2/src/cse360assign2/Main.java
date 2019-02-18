06cb75a7cc5b36e3ea7bd8c7acb79fb2f5263cf7
hsinjlee <rlee4408@gmail.com>
2019-02-18 13:50:56 -0700

    Create the third version updated 1

--- CSE360a2/src/cse360assign2/Main.java
+++ CSE360a2/src/cse360assign2/Main.java
@@ -1,28 +1,38 @@
 package cse360assign2;
 
+/**
+ * Student name : Hsin-Jung Lee
+ * Class ID: 451
+ * Assignment 2
+ * Description: This program will calculator numbers and print history.
+ * https://github.com/hsinjlee/cse360assign2
+ */
+
+/**
+ * main class test Calculator java file
+ */
 public class Main {
     public static void main(String[] args) {
-        Calculator c = new Calculator();
+        Calculator myCalculator = new Calculator();
 
-        c.add(3);
-        c.subtract(1);
-        c.multiply(2);
-        c.divide(3);
-        c.divide(0);
-        c.add(1);
-        c.multiply(3);
-        c.divide(2);
-        c.divide(-2);
-        c.divide(0);
-        c.divide(20);
-        c.add(100);
-        c.subtract(-101);
-        c.subtract(202);
-        c.multiply(5);
-        c.divide(-4);
-        c.divide(0);
-        System.out.println(c.getTotal());
-        System.out.println(c.getHistory());
-
+        myCalculator.add(3);
+        myCalculator.subtract(1);
+        myCalculator.multiply(2);
+        myCalculator.divide(3);
+        myCalculator.divide(0);
+        myCalculator.add(1);
+        myCalculator.multiply(3);
+        myCalculator.divide(2);
+        myCalculator.divide(-2);
+        myCalculator.divide(0);
+        myCalculator.divide(20);
+        myCalculator.add(100);
+        myCalculator.subtract(-101);
+        myCalculator.subtract(202);
+        myCalculator.multiply(5);
+        myCalculator.divide(-4);
+        myCalculator.divide(0);
+        System.out.println(myCalculator.getTotal());
+        System.out.println(myCalculator.getHistory());
     }
 }
